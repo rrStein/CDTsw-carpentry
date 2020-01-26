00beb02171512009e926bac685773985b2ce4c65
rrStein <rrstein521@gmail.com>
2020-01-26 14:02:09 +0000

    fix

---
+++ example-hello-world/hello.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# Prints a greeting message to the screen
+
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo semper elit, non semper tortor.
+# Quisque fringilla metus sem, id faucibus ipsum auctor vel. Aliquam consectetur od
+# io eu erat pulvinar lacinia. Maecenas a lectus vitae felis placerat euismod sed ut felis.
+# Suspendisse potenti. Phasellus ullamcorper dictum neque eu accumsan. Nulla eros dui, molestie
+# non ultrices at, finibus vitae lectus. Pellentesque vitae odio ac mi congue ultricies
+# eget eleifend risus. Fusce venenatis leo et luctus aliquam. Fusce mattis diam quis lacus ultrices
+# placerat quis in elit. Maecenas eleifend, urna id porttitor placerat, ante leo pharetra erat,
+# in pretium odio augue vel lorem. Nullam consectetur faucibus nisl ut condimentum.
+
+# Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla a nisl diam.
+# Quisque fringilla metus sem, id faucibus ipsum auctor vel. Aliquam consectetur od
+# io eu erat pulvinar lacinia. Maecenas a lectus vitae felis placerat euismod sed ut felis.
+# Suspendisse potenti. Phasellus ullamcorper dictum neque eu accumsan. Nulla eros dui, molestie
+# non ultrices at, finibus vitae lectus. Pellentesque vitae odio ac mi congue ultricies
+# eget eleifend risus. Fusce venenatis leo et luctus aliquam. Fusce mattis diam quis lacus ultrices
+# placerat quis in elit. Maecenas eleifend, urna id porttitor placerat, ante leo pharetra erat,
+# in pretium odio augue vel lorem. Nullam consectetur faucibus nisl ut condimentum.
+
+# Proin feugiat rutrum viverra. Cras laoreet eleifend nulla non auctor. Integer lorem leo.
+# Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla a nisl diam.
+# Quisque fringilla metus sem, id faucibus ipsum auctor vel. Aliquam consectetur od
+# io eu erat pulvinar lacinia. Maecenas a lectus vitae felis placerat euismod sed ut felis.
+# Suspendisse potenti. Phasellus ullamcorper dictum neque eu accumsan. Nulla eros dui, molestie
+# non ultrices at, finibus vitae lectus. Pellentesque vitae odio ac mi congue ultricies
+# eget eleifend risus. Fusce venenatis leo et luctus aliquam. Fusce mattis diam quis lacus ultrices
+# placerat quis in elit. Maecenas eleifend, urna id porttitor placerat, ante leo pharetra erat,
+# in pretium odio augue vel lorem. Nullam consectetur faucibus nisl ut condimentum.
+
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus.
+# non metus a nibh tempor porttitor vel sed elit..
+# Aenean in purus feugiat, laoreet enim eu, ultricies sem..
+# Curabitur in fringilla turpis. Praesent volutpat sem odio, sit.
+# amet aliquet odio eleifend hendrerit. In sit amet aliquam.
+# nisl. Fusce et augue aliquet, commodo quam nec, ultricies.
+# purus. Proin sit amet leo neque. Nam vulputate quis.
+# tellus sed eleifend. Cras sed nibh vehicula, euismod eros.
+# vitae, placerat nisi. Phasellus in ullamcorper ante. Aliquam dictum.
+# cursus libero, ac posuere sapien consequat sit amet. Nunc.
+# ut diam posuere, laoreet neque ac, faucibus est.
+
+echo "Greetings from git training."
+echom "Hello, World!"
+
+# Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla a nisl diam.
+# Quisque fringilla metus sem, id faucibus ipsum auctor vel. Aliquam consectetur od
+# io eu erat pulvinar lacinia. Maecenas a lectus vitae felis placerat euismod sed ut felis.
+# Suspendisse potenti. Phasellus ullamcorper dictum neque eu accumsan. Nulla eros dui, molestie
+# non ultrices at, finibus vitae lectus. Pellentesque vitae odio ac mi congue ultricies
+# eget eleifend risus. Fusce venenatis leo et luctus aliquam. Fusce mattis diam quis lacus ultrices
+# placerat quis in elit. Maecenas eleifend, urna id porttitor placerat, ante leo pharetra erat,
+# in pretium odio augue vel lorem. Nullam consectetur faucibus nisl ut condimentum.
+
+# Proin feugiat rutrum viverra. Cras laoreet eleifend nulla non auctor. Integer lorem leo.
+# Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla a nisl diam.
+# Quisque fringilla metus sem, id faucibus ipsum auctor vel. Aliquam consectetur od
+# io eu erat pulvinar lacinia. Maecenas a lectus vitae felis placerat euismod sed ut felis.
+# Suspendisse potenti. Phasellus ullamcorper dictum neque eu accumsan. Nulla eros dui, molestie
+# non ultrices at, finibus vitae lectus. Pellentesque vitae odio ac mi congue ultricies
+# eget eleifend risus. Fusce venenatis leo et luctus aliquam. Fusce mattis diam quis lacus ultrices
+# placerat quis in elit. Maecenas eleifend, urna id porttitor placerat, ante leo pharetra erat,
+# in pretium odio augue vel lorem. Nullam consectetur faucibus nisl ut condimentum.
+
